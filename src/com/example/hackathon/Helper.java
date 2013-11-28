00dd5981fcde9aa96f9b6726903bcf56a7889856
AnshulBansal <ans_hul_1994@yahoo.co.in>
2013-11-28 16:55:38 +0530

    Random listing done

--- src/com/example/hackathon/Helper.java
+++ src/com/example/hackathon/Helper.java
@@ -18,12 +18,13 @@ import android.util.Log;
 
 
 public class Helper {
-	ArrayList artistlisting;
+	static ArrayList<String> artistlisting = new ArrayList<String>();
+	static ArrayList<String> tracklisting = new ArrayList<String>();
 	static HttpClient client=new DefaultHttpClient();
 	
-	public static ArrayList getartistlisting(int genreid) throws ClientProtocolException, IOException, JSONException{
-		
-		
+	public static ArrayList<String> getartistlisting(int genreid) throws ClientProtocolException, IOException, JSONException{
+		JSONArray	jsonArray= new JSONArray();
+	JSONObject object;	
 		String Url = "http://restservice.gaana.com/index.php?type=genre&subtype=genre_artist_listing&genre_id=";
 		StringBuilder sb = new StringBuilder(Url);
 		sb.append(genreid);
@@ -36,21 +37,62 @@ public class Helper {
 			HttpEntity e = r.getEntity();
 			String data = EntityUtils.toString(e);
 			JSONObject timeline = new JSONObject(data);
-		JSONArray	jsonArray = timeline.getJSONArray("artist");
+			jsonArray = timeline.getJSONArray("artist");
 Log.d("ArtistDetail", jsonArray.toString());
+int i=0;
+for(i=0;i<jsonArray.length();i++){
+	object = jsonArray.getJSONObject(i);
+	artistlisting.add(object.getString("artist_id"));
+	
+	
+	
+	
+}
 			//return jsonArray;
 		}
 
 		
-		return null;
+		return artistlisting;
 
 		
 		
 		
 	}
-	
-	
 
+	public static ArrayList<String> gettracklisting(String[] arg0, String artistid) throws ClientProtocolException, IOException, JSONException {
+		// TODO Auto-generated method stub
+		JSONArray	jsonArray= new JSONArray();
+		JSONObject object;	
+		
+			String Url = "http://restservice.gaana.com/index.php?type=artist&subtype=artist_track_listing&artist_id=";
+			StringBuilder sb = new StringBuilder(Url);
+			sb.append(artistid);
+			sb.append("&genre_id=");
+			sb.append(arg0[0]);
+			sb.append("&limit=10");
+			
+			HttpGet get = new HttpGet(sb.toString());
+			HttpResponse r = client.execute(get);
+			int status = r.getStatusLine().getStatusCode();
+			if (status == 200) {
+				HttpEntity e = r.getEntity();
+				String data = EntityUtils.toString(e);
+				JSONObject timeline = new JSONObject(data);
+				jsonArray = timeline.getJSONArray("tracks");
+	Log.d("TrackDetail", jsonArray.toString());
+	int i=0;
+	for(i=0;i<jsonArray.length();i++){
+		object = jsonArray.getJSONObject(i);
+		tracklisting.add(object.getString("track_id"));
+		
+		
+		
+		
+	}	
+	}
 	
+	return tracklisting;
+
+	}
 
 }
--- src/com/example/hackathon/MainActivity.java
+++ src/com/example/hackathon/MainActivity.java
@@ -1,21 +1,34 @@
 package com.example.hackathon;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.apache.http.client.ClientProtocolException;
+import org.json.JSONException;
+
 import android.app.Activity;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
 
 public class MainActivity extends Activity implements OnClickListener {
 
 	
-	
-	
+	ArrayList<String> artistlisting = new ArrayList<String>();
+
+	ArrayList<String> Tracklisting = new ArrayList<String>();
+	TextView tvtest;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
+		tvtest = (TextView) findViewById(R.id.textView1);
+		new Read().execute("56");
+		
 	}
 
 	@Override
@@ -25,4 +38,75 @@ public class MainActivity extends Activity implements OnClickListener {
 	}
 
 	
+	
+	
+
+
+	class Read extends AsyncTask<String, Integer, String> {
+
+		@Override
+		protected void onPostExecute(String result) {
+			// TODO Auto-generated method stub
+			super.onPostExecute(result);
+			tvtest.setText(result);
+		}
+
+		@Override
+		protected String doInBackground(String[] arg0) {
+			// TODO Auto-generated method stub
+			try {
+				artistlisting =Helper.getartistlisting(Integer.parseInt(arg0[0]));
+				int number =  (int) ((int) 10*Math.random());
+				ArrayList<String> RawTracklisting = new ArrayList<String>();
+				RawTracklisting = Helper.gettracklisting(arg0,artistlisting.get(number));
+				number =  (int) ((int) 10*Math.random());
+				RawTracklisting.addAll(Helper.gettracklisting(arg0,artistlisting.get(number)));
+				number =  (int) ((int) 10*Math.random());
+				RawTracklisting.addAll(Helper.gettracklisting(arg0,artistlisting.get(number)));
+				int i=0;
+				for(i=0;i<11;i++){
+					number =  (int) ((int) RawTracklisting.size()*Math.random());
+					if(!(Tracklisting.contains(RawTracklisting.get(number)))){
+					Tracklisting.add(RawTracklisting.get(number));
+					}else{
+						i--;
+					}
+					
+				}
+				
+				
+				
+						return Tracklisting.toString();
+			} catch (NumberFormatException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (JSONException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+			
+			return null;
+		}
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 }
+
+
+
+
+
+
